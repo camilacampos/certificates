8658c9007fd0bfca4fe7694caed02f5014a6bb0c
camilacampos <camilacamposramos.si@gmail.com>
2018-11-22 00:41:08 -0200

    :moises: 2
    Signed-off-by: camilacampos <camilacamposramos.si@gmail.com>

--- app/helpers/certificates_helper.rb
+++ app/helpers/certificates_helper.rb
@@ -1,6 +1,6 @@
 module CertificatesHelper
   def formatar_data(start_date, end_date)
-    "o dia #{start_date.strftime('%d/%m/%Y')}" if end_date.nil? || start_date == end_date
+    return "o dia #{start_date.strftime('%d/%m/%Y')}" if end_date.nil? || start_date == end_date
 
     "o período de #{start_date.strftime('%d/%m/%Y')} à #{end_date.strftime('%d/%m/%Y')}"
   end
