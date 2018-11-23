1c93e3d693574ff6e79fddb2bfabbcf0e87f25f9
camilacampos <camilacamposramos.si@gmail.com>
2018-11-22 23:04:39 -0200

    melhorando texto do certificado
    Signed-off-by: camilacampos <camilacamposramos.si@gmail.com>

--- app/helpers/certificates_helper.rb
+++ app/helpers/certificates_helper.rb
@@ -1,7 +1,7 @@
 module CertificatesHelper
   def formatar_data(start_date, end_date)
-    return "o dia #{start_date.strftime('%d/%m/%Y')}" if end_date.nil? || start_date == end_date
+    return "no dia #{start_date.strftime('%d/%m/%Y')}" if end_date.nil? || start_date == end_date
 
-    "o período de #{start_date.strftime('%d/%m/%Y')} à #{end_date.strftime('%d/%m/%Y')}"
+    "entre os dias #{start_date.strftime('%d/%m/%Y')} e #{end_date.strftime('%d/%m/%Y')}"
   end
 end
